62e2b61e59f0281f4079a3b06df0db8881f99893
Taro Segawa(瀬川太朗） <tdog7305@icloud.com>
2022-05-22 13:44:16 +0900

    Initial Commit

---
+++ mePhotoMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mePhotoMap
+//
+//  Created by 瀬川太朗 on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
